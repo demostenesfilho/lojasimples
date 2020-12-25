7c209b669f4d11af505bb94127146cd784611958
Demóstenes Filho <58401504+demostenesfilho@users.noreply.github.com>
2020-12-24 22:36:40 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="css/style.css" />
+    <title>SimpleStore</title>
+</head>
+<body>
+    <nav class="navbar">
+        <a href="#" class="titulo">Loja simples</a>
+        <button class="botaocarrinho">Carrinho</button>
+    </nav>
+
+    <div class="card">
+        <div class="imagecard">
+            <img class="imagem" src="images/camisa1.png" />
+        </div>
+        <p class="nomedoproduto">Nome do produto</p>
+        <div class="precocampo">
+            <p class="preco">R$15</p>
+            <button class="detalhes">Detalhes</button>
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="imagecard">
+            <img class="imagem" src="images/camisa2.png" />
+        </div>
+        <p class="nomedoproduto">Nome do produto</p>
+        <div class="precocampo">
+            <p class="preco">R$15</p>
+            <button class="detalhes">Detalhes</button>
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="imagecard">
+            <img class="imagem" src="images/camisa3.png" />
+        </div>
+        <p class="nomedoproduto">Nome do produto</p>
+        <div class="precocampo">
+            <p class="preco">R$15</p>
+            <button class="detalhes">Detalhes</button>
+        </div>
+    </div>
+
+
+
+
+
+
+</body>
+</html>
